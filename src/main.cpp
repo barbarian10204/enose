40995cbd2d54b7f73a1f671b25856bfeb1611dac
Ventsislav <hartien4orap@gmail.com>
2025-11-23 09:55:56 +0100

    Implemented the bluetooth emitter control function

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include <Arduino.h>
 #include <HardwareSerial.h>
 #include <SIM800L.h>
+#include <ArduinoBLE.h>        	    // Arduino BLE library
 #include "Multichannel_Gas_GMXXX.h" // Grove Multichannel Gas Sensor v2
 #include "seeed_bme680.h"           // Seeed BME680
 #include "SensirionI2CSgp41.h"      // Seeed SGP41
-#include <ArduinoBLE.h>        	    // Arduino BLE library
 // Simple USART communication example for Arduino Nano ESP32
 
 // Constants
@@ -35,14 +35,15 @@ uint8_t emitterBytes[8] = {0, 0, 0, 0, 0, 0, 0, 0}; // <-- control bytes for emi
 
 // Function prototypes
 // Function prototypes: Init functions
-void init_GSM();		// innitialises the communication with server
-bool init_bluetooth();		// innitialises the bluetooth module
-bool init_sensors();		// starts the sensors
-bool init_emmi();		// starts the emmiter (why ??)
+void init_GSM();				// innitialises the communication between GSM and server
+bool init_bluetooth();			// innitialises the bluetooth module
+bool init_sensors();			// starts the sensors
 bool send_error(error_codes_t); // sends the error code to server
 
 // Function prototypes: Main functions
-void sensor_readings(); 	// reads data from sensors
+void sensor_readings(); 				// reads data from sensors
+bool bluetooth_to_emitter();		// sends control bytes to emitter over bluetooth
+bool controlEmitters(BLEDevice peripheral);		// controls the emitter over bluetooth
 
 void setup() {
 	// set up leds before inits so we can indicate if any of them failed
@@ -57,13 +58,114 @@ void loop() {
 	sensor_readings();
 
 	// wrap every other action in a timer-activated block (active after 30 minutes of collecting readings to preheat gas sensors)
-	
+
+	if (bluetooth_to_emitter()) {
+		Serial.println("Emitter control successful");
+	} else {
+		Serial.println("No emitter controlled");
+	}
 }
 
 // Functions Definitions
 // Functions Definitions: Main functions
 void sensor_readings() {
 	// read data from sensors
+}
+
+bool bluetooth_to_emitter() {
+	// check if a peripheral has been discovered
+	BLEDevice peripheral = BLE.available();
+
+	if (peripheral) {
+		// discovered a peripheral, print out address, local name, and advertised service
+		Serial.print("Found ");
+		Serial.print(peripheral.address());
+		Serial.print(" '");
+		Serial.print(peripheral.localName());
+		Serial.print("' ");
+		Serial.print(peripheral.advertisedServiceUuid());
+		Serial.println();
+
+		if (peripheral.localName() != "EmitterDevice") {
+		return false;
+		}
+
+		// stop scanning
+		BLE.stopScan();
+
+		if (controlEmitters(peripheral)) {
+			Serial.println("Emitter control successful");
+			return true;
+		} else {
+			Serial.println("Peripheral disconnected");
+			BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214"); // restart scanning
+			return false;
+		}
+	}
+	return false;
+}
+
+bool controlEmitters(BLEDevice peripheral){
+// connect to the peripheral
+  Serial.println("Connecting ...");
+
+  if (peripheral.connect()) {
+    Serial.println("Connected");
+  } else {
+    Serial.println("Failed to connect!");
+    return false;
+  }
+
+  // discover peripheral attributes
+  Serial.println("Discovering attributes ...");
+  if (peripheral.discoverAttributes()) {
+    Serial.println("Attributes discovered");
+  } else {
+    Serial.println("Attribute discovery failed!");
+    peripheral.disconnect();
+    return;
+  }
+
+  // retrieve the Emitter characteristic
+  BLECharacteristic emittersCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1214");
+
+  if (!emittersCharacteristic) {
+    Serial.println("Peripheral does not have Emitter characteristic!");
+    peripheral.disconnect();
+    return false;
+  } else if (!emittersCharacteristic.canWrite()) {
+    Serial.println("Peripheral does not have a writable Emitter characteristic!");
+    peripheral.disconnect();
+    return false;
+  }
+
+  	if (peripheral.connected()) {
+		// while the peripheral is connected
+
+		// Print what we're about to send
+		Serial.print("Bytes: ");
+		for (int i = 0; i < 8; i++) {
+		Serial.print(emitterBytes[i]);
+		if (i < 7) Serial.print(", ");
+		}
+		Serial.println();
+
+		// write the 8-byte array to the characteristic
+		// writeValue accepts (const uint8_t* data, unsigned int length)
+		bool ok = emittersCharacteristic.writeValue(emitterBytes, 8);
+		if (ok) {
+		Serial.println("Write successful");
+		} else {
+		Serial.println("Write failed");
+		}
+		return true;
+	}
+	else {
+		Serial.println("Peripheral disconnected");
+		return false;
+	}
+
+	return false;
 }
 
 // Functions Definitions: Init functions
@@ -81,7 +183,6 @@ bool init_bluetooth() {
 	BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
 	return true;
 }
-bool init_emmi() {} // why ??
 
 bool init_sensors() {
 
